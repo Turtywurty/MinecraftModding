24694e99b6206612a3df1b58999b55f3ff03c46e
Tebreca <ewan.arends10@gmail.com>
2018-03-02 10:25:23 +0100

    cleanup code

--- java/harry/mod/util/handlers/RegistryHandler.java
+++ java/harry/mod/util/handlers/RegistryHandler.java
@@ -33,7 +33,7 @@ public class RegistryHandler
 	public static void onBlockRegister(RegistryEvent.Register<Block> event)
 	{
 		event.getRegistry().registerAll(BlockInit.BLOCKS.toArray(new Block[0]));
-		TileEntityHandler.registerTileEntities();
+		Main.proxy.registerTileEntities();
 	}
 	
 	@SubscribeEvent
--- java/harry/mod/util/handlers/TileEntityHandler.java
+++ java/harry/mod/util/handlers/TileEntityHandler.java
@@ -1,5 +1,6 @@
 package harry.mod.util.handlers;
 
+import harry.mod.objects.blocks.machines.electricalSinterer.TileEntityElectricalSinteringFurnace;
 import harry.mod.objects.blocks.machines.sinterer.TileEntitySinteringFurnace;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -8,5 +9,6 @@ public class TileEntityHandler
 	public static void registerTileEntities()
 	{
 		GameRegistry.registerTileEntity(TileEntitySinteringFurnace.class, "sintering_furnace");
+		GameRegistry.registerTileEntity(TileEntityElectricalSinteringFurnace.class, "e_sintering_furnace");
 	}
 }
