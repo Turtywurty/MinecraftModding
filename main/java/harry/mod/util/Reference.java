ab832231a9c3f3136c41b1fb961af2bf0bb3df7c
HarryTechRevs <30769752+HarryTechRevs@users.noreply.github.com>
2018-08-09 16:24:42 +0100

    Add files via upload

--- main/java/harry/mod/util/Reference.java
+++ main/java/harry/mod/util/Reference.java
@@ -1,15 +1,13 @@
-package harry.mod.util;
+package harry.mods.tutorialmod;
 
 public class Reference 
 {
-	public static final String MODID = "tm";
-	public static final String NAME = "Tutorial Mod";
-	public static final String VERSION = "1.0";
-	public static final String CLIENT = "harry.mod.proxy.ClientProxy";
-	public static final String COMMON = "hary.mod.proxy.CommonProxy";
+	public static final String MODID = "tutorial";
+	public static final String MODNAME = "Tutorial Mod";
+	public static final String VERSION = "1.12.2";
 	
-	public static final int ENTITY_CENTAUR = 120;
-	public static final int ENTITY_ARION = 121;
+	public static final String CLIENT = "harry.mods.tutorialmod.proxy.ClientProxy";
+	public static final String SERVER = "harry.mods.tutorialmod.proxy.CommonProxy";
 	
-	public static final int GUI_SINTERING_FURNACE = 0;
+	public static final int GUI_COPPER_CHEST = 2;
 }
